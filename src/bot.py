32e4d0f79644e631772f569e4a4fa37d965aea3d
VissuManchem <1vissu1@gmail.com>
2024-05-18 21:57:25 -0400

    get types

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
-
+import requests
 dotenv_path = "../.env" # insert your .env path here
 load_dotenv(dotenv_path)
 
@@ -30,5 +30,27 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('!gettype'):
+        # splits the command into an array of strings
+        mon = message.content.split()
+
+        # when len > 1 means there is a word after !gettype
+        if len(mon) > 1:
+            poke = mon[1]
+            # sends a request to the url and parses JSON content into python
+            api = requests.get("https://pokeapi.co/api/v2/pokemon/" + poke)
+            conv = api.json()
+
+            # status code 200 means the pokemon exists
+            if api.status_code == 200:
+                #
+                types = [type_info['type']['name'] for type_info in conv['types']]
+                types_str = ', '.join(types)
+                await message.channel.send(types_str)
+            else:
+                await message.channel.send(f'{poke} is not a pokemon, please try again.')
+        else:
+            await message.channel.send('Type a pokemon name after')
 
 bot.run(DISCORD_TOKEN)
--- src/pokeapi.py
+++ src/pokeapi.py
@@ -6,3 +6,7 @@ import requests
 TODO: Implement a function that returns a Pokémon's type based on the Pokémon's name and prints it to console
 using the requests library to interact with the Pokémon API
 """
+@bot_event
+async def on_message(message):
+    if message.author == bot.user:
+        return
