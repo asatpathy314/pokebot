38f939665ee817ba6ad64705c834624ae564bd94
asatpathy314 <asatpathy314@gmail.com>
2024-05-18 21:02:31 -0400

    added TODO

--- src/pokeapi.py
+++ src/pokeapi.py
@@ -1,3 +1,8 @@
 # This file we interact with the Pokémon API
 
 import requests
+
+"""
+TODO: Implement a function that returns a Pokémon's type based on the Pokémon's name and prints it to console
+using the requests library to interact with the Pokémon API
+"""
